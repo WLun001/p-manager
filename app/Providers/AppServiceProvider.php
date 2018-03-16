d422aff76edc2ec8d6d6eeb5b5370614ce9ea691
Wei Lun <weilun_95@hotmail.com>
2018-03-16 16:42:08 +0800

    added comment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        // change the defaul string length
         Schema::defaultStringLength(191);
     }
 
