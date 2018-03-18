4bba931c378412afa52951ff5ae62e376d8e4ac3
Wei Lun <weilun_95@hotmail.com>
2018-03-18 19:01:24 +0800

    reposition foreach to have items display horizontally

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -8,21 +8,20 @@
         <!-- <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p> -->
       </div>
 
+     <div class="row">
      @foreach($company->projects as $project)
-      <div class="row">
         <div class="col-lg-4">
           <h2>{{ $project->name }}</h2>
           <p class="text-danger">As of v9ur broeshing.</p>
           <p>{{ $project->description}} </p>
           <p><a class="btn btn-primary" href="/projects/show/{{ $project->id }}" role="button">View project details »</a></p>
         </div>
-      </div>
       @endforeach
+      </div>
 
       <!-- Site footer -->
       <footer class="footer">
         <p>© 2016 Company, Inc.</p>
       </footer>
-
 
 @endsection 
