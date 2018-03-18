32fd3f426e3983f0852cd8ea8cee4c753e42cbe0
Wei Lun <weilun_95@hotmail.com>
2018-03-18 18:39:01 +0800

    added company description

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -7,8 +7,8 @@
       <!-- Jumbotron -->
       <div class="jumbotron">
         <h1>{{ $company->name }}</h1>
-        <p class="lead">Cras justo odio, dapibus ac facilisis in, egestas eget quam. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet.</p>
-        <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p>
+        <p class="lead">{{ $company->description }}</p>
+        <!-- <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p> -->
       </div>
 
       <!-- Example row of columns -->
