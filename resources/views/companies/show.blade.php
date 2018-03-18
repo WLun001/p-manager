455126a0215120caa6edcceb9955ddfaa5d6dac3
Wei Lun <weilun_95@hotmail.com>
2018-03-18 19:23:44 +0800

    added side bar on company view

--- resources/views/companies/show.blade.php
+++ resources/views/companies/show.blade.php
@@ -1,14 +1,15 @@
 @extends('layouts.app')
 
 @section('content')
-      <!-- Jumbotron -->
+
+    <div class="col-sm-3 col-md-9 col-lg-9 pull-left">
       <div class="jumbotron">
         <h1>{{ $company->name }}</h1>
         <p class="lead">{{ $company->description }}</p>
         <!-- <p><a class="btn btn-lg btn-success" href="#" role="button">Get started today</a></p> -->
       </div>
 
-     <div class="row">
+     <div class="row" style="background:white; margin: 4px;">
      @foreach($company->projects as $project)
         <div class="col-lg-4">
           <h2>{{ $project->name }}</h2>
@@ -18,6 +19,42 @@
         </div>
       @endforeach
       </div>
+    </div>
+
+      <!-- side bar -->
+      <div class="col-sm-3 col-md-3 col-lg-3 pull-right">
+          <!-- <div class="sidebar-module sidebar-module-inset">
+            <h4>About</h4>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+          </div> -->
+
+         <div class="sidebar-module">
+            <h4>Actions</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">Edit</a></li>
+              <li><a href="#">Delete</a></li>
+              <li><a href="#">Add New Member</a></li>
+            </ol>
+          </div>
+          <div class="sidebar-module">
+            <h4>Members</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">March 2014</a></li>
+              <li><a href="#">February 2014</a></li>
+              <li><a href="#">January 2014</a></li>
+              <li><a href="#">December 2013</a></li>
+              <li><a href="#">November 2013</a></li>
+              <li><a href="#">October 2013</a></li>
+              <li><a href="#">September 2013</a></li>
+              <li><a href="#">August 2013</a></li>
+              <li><a href="#">July 2013</a></li>
+              <li><a href="#">June 2013</a></li>
+              <li><a href="#">May 2013</a></li>
+              <li><a href="#">April 2013</a></li>
+            </ol>
+          </div>
+     
+        </div>
 
       <!-- Site footer -->
       <footer class="footer">
